35b5724989d5f11bd9c7c595d5f8c8c541e597e1
Rafael Pires <rafael.pires@epfl.ch>
2026-03-12 11:17:00 +0100

    pics

--- vendor/jquery/jquery.random-images.js
+++ vendor/jquery/jquery.random-images.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
 
-var images=['img/2026_img001.jpg',
-                'img/2026_img002.jpg',
-                'img/2026_img003.jpg',
-                'img/2026_img004.jpg',
-                'img/2026_img005.jpg',
-                'img/2026_img006.jpg',
-                'img/2026_img007.jpg',
-                'img/2026_img008.jpg'];
+var images=['img/2027_img001.jpg',
+                'img/2027_img002.jpg',
+                'img/2027_img003.jpg',
+                'img/2027_img004.jpg',
+                'img/2027_img005.jpg'];
 
 var randomNumber = Math.floor(Math.random() * images.length);
 var bgImg = 'url(' + images[randomNumber] + ')';
