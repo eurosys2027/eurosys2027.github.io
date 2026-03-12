dbca3eb9e6f6d9f1107d584a86915a699df9aa83
TAGHDOUTI <taghdouti.b@gmail.com>
2026-03-12 13:30:37 +0000

    change random.js

--- vendor/jquery/jquery.random-images.js
+++ vendor/jquery/jquery.random-images.js
@@ -4,7 +4,13 @@ var images=['img/2027_img001.jpg',
                 'img/2027_img002.jpg',
                 'img/2027_img003.jpg',
                 'img/2027_img004.jpg',
-                'img/2027_img005.jpg'];
+                'img/2027_img005.jpg',
+                'img/2027_img006.jpg',
+                'img/2027_img007.jpg',
+                'img/2027_img008.jpg',
+                'img/2027_img009.jpg',
+                'img/2027_img010.jpg',
+                'img/2027_img011.jpg'  ];
 
 var randomNumber = Math.floor(Math.random() * images.length);
 var bgImg = 'url(' + images[randomNumber] + ')';
